08401979bf718c31c2186d6cbbb63249ca5a3b8c
Thilina Lakshitha <lakshithathilina74@gmail.com>
2025-09-14 22:52:12 +0530

    add router

--- BackEnd/my-app/Route/ProductRoutes.js
+++ BackEnd/my-app/Route/ProductRoutes.js
@@ -69,6 +69,16 @@ router.get("/:id/image", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 // Delete a product by ID
 router.delete("/:id", async (req, res) => {
